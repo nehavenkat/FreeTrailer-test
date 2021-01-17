26cd698c430da8e4e9da7d34db0451f74ec4850c
neha venkat <venkatsetty.neha@gmail.com>
2021-01-17 17:35:35 +0100

    updated

--- App.js
+++ App.js
@@ -1,26 +1,28 @@
-import React ,{Component} from 'react';
-import Landing from './component/Landing' ;
-import Home from './component/Home' ;
+import React, { Component } from 'react'
+import Landing from './screens/Landing' 
+import Home from "./screens/Home"
 
-export default class App extends Component {
-constructor(props){
- super(props)
- this.state = {
-  component : <Landing />
- }
-}
-componentDidMount(){
-     this.timeoutHandle = setTimeout(()=>{
-          this.setState({ component: <Home /> })
-     }, 1000);
-}
-componentWillUnmount(){
-     clearTimeout(this.timeoutHandle); 
-}
 
-render() {
-return (
-  this.state.component
-);
+class App extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+     component : <Landing />
+    }
+   }
+   componentDidMount(){
+        this.timeoutHandle = setTimeout(()=>{
+             this.setState({ component: <Home /> })
+        }, 4000);
+   }
+   componentWillUnmount(){
+        clearTimeout(this.timeoutHandle); 
+   }
+  render() {
+    return (
+      this.state.component
+      
+    );
+  }
 }
-}
+export default App;
